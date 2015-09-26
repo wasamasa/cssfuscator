ce7ec0f5acc64365f0cc0ea4dd6ed0a818aec88f
Vasilij Schneidermann <v.schneidermann@gmail.com>
2015-09-26 17:34:24 +0200

    Accumulate changes to avoid trailing comma

--- cssfuscator.scm
+++ cssfuscator.scm
@@ -5,28 +5,32 @@
 ;; TODO optimizations (like, turning #ffffff into #fff)?
 ;; TODO allow options for splitting out css or pretty-printing or naming
 
+(define (hex-code-at image x y)
+  (receive (r g b a)
+      (image-pixel/rgba image x y)
+    ;; is the pixel fully opaque?
+    (if (= a 255)
+        ;; FIXME don't hardcode coordinates
+        ;; FIXME don't hardcode scaling factor
+        (format #f "~dpx ~dpx #~2,'0x~2,'0x~2,'0x" x y r g b)
+        #f)))
+
 (define (transform image-path)
   (let* ((image (image-load image-path))
          (width (image-width image))
-         (height (image-height image)))
-    ;; NOTE one way to fix the trailing comma issue would be
-    ;; accumulating strings into a list and concatenating it
-    (let y-loop ((y 0))
-      (when (< y height)
-        (let x-loop ((x 0))
-          (when (< x width)
-            (receive (r g b a) (image-pixel/rgba image x y)
-              ;; is the pixel fully opaque?
-              (when (= a 255)
-                  ;; FIXME don't hardcode coordinates
-                  ;; FIXME don't hardcode scaling factor
-                  ;; FIXME Firefox doesn't permit a final trailing
-                  ;; comma, either semicolon or nothing are OK
-                  (display (format #f "~dpx ~dpx #~2,'0x,~2,'0x,~2,'0x"
-                                   x y r g b))))
-            (x-loop (add1 x))))
-        (y-loop (add1 y))))
-    (image-destroy image)))
+         (height (image-height image))
+         (data (let y-loop ((y 0) (acc '()))
+                 (if (< y height)
+                     (let x-loop ((x 0) (acc acc))
+                       (if (< x width)
+                           (let ((hex-code (hex-code-at image x y)))
+                             (if hex-code
+                                 (x-loop (add1 x) (cons hex-code acc))
+                                 (x-loop (add1 x) acc)))
+                           (y-loop (add1 y) acc)))
+                     (string-intersperse (reverse acc) ",")))))
+    (image-destroy image)
+    data))
 
 (define (error-message message)
   (with-output-to-port (current-error-port)
@@ -58,7 +62,7 @@
         (lambda ()
           ;; FIXME don't hardcode units
           (display "<!DOCTYPE html><html><head><title>Image</title><style type=\"text/css\">#image{width:1px;height:1px;box-shadow:")
-          (transform input-file)
+          (display (transform input-file))
           (display "}</style></head><body><div id=\"image\"></div></body></html>"))))))
 
 (main)
